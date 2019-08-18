05523c735813db1c64c86afad8530fc600cfad93
Ben Francisco <bpfranci@math.tamu.edu>
2019-08-18 21:51:13 +0000

    Automatically backed up by Learn

--- lib/until.rb
+++ lib/until.rb
@@ -2,10 +2,9 @@ def using_until
   levitation_force = 6
   # loop until levitation force is 10
   until levitation_force == 10
-    # every time the string "Wingardium Leviosa" is printed out levitation force 
+    # every time the string "Wingardium Leviosa" is printed out levitation force
     # q is increased by 1.
     puts "Wingardium Leviosa"
     levitation_force += 1
   end
 end
-
